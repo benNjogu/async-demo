1d24dc26ee1ecbe16089301bfe572de189726dec
benNjogu <kamandemachafi@gmail.com>
2022-10-07 14:21:58 +0300

    Patterns for dealing with asynchronous code

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 console.log("Before");
+const user = getUser(1);
+console.log(user);
 console.log("After");
+
+//Callbacks
+//Promises
+//Async/await
+
+function getUser(id) {
+  setTimeout(() => {
+    console.log("Reading a user from a database...");
+    return { id: id, gitHubUserName: "Ben" };
+  }, 2000);
+}
