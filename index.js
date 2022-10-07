1b177221fed25b14205f430269158a6389d74734
benNjogu <kamandemachafi@gmail.com>
2022-10-07 21:30:28 +0300

    Async and Await

--- index.js
+++ index.js
@@ -9,11 +9,25 @@ console.log("Before");
   });
 });*/
 
-const p = getUser(1);
+/*const p = getUser(1);
 p.then(user => getRepositories(user.gitHubUserName))
     .then(repos => getCommits(repos[0]))
     .then(commits => console.log('Commits', commits))
-    .catch(err => console.log('Error', err.message));
+    .catch(err => console.log('Error', err.message));*/
+
+//Async and Await approach
+async function displayCommits(){
+    try {
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUserName);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    } catch (error) {
+        console.log('Error ', error);
+    }
+}
+
+displayCommits();
 
 console.log("After");
 
@@ -30,7 +44,8 @@ function getRepositories(username) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Calling Github API...");
-      resolve(["repo1", "repo2", "repo3"]);
+      //resolve(["repo1", "repo2", "repo3"]);
+      reject(new Error('Could not get the repos'))
     }, 2000);
   });
 }
